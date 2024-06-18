cdbb39c218b96a49dbe0f941fd7d81e22f6b6c51
Steve Aster AFOVO <72698973+SteveAsterAfovo@users.noreply.github.com>
2024-06-18 16:51:31 +0100

    Create database.js

---
+++ backend/src/config/database.js
@@ -0,0 +1,21 @@
+// src/config/database.js
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
