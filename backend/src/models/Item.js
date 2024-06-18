0686e8fb79a3265cddb5f5d1c4d2238ee24fbdd7
Steve Aster AFOVO <72698973+SteveAsterAfovo@users.noreply.github.com>
2024-06-18 16:47:32 +0100

    Create Item.js

---
+++ backend/src/models/Item.js
@@ -0,0 +1,32 @@
+// src/models/Item.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  seller: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Item', itemSchema);
