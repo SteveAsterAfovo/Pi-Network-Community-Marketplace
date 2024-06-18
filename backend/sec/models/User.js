6da8a45f359e7cebaea53274dcc21bc83e221958
Steve Aster AFOVO <72698973+SteveAsterAfovo@users.noreply.github.com>
2024-06-18 16:43:53 +0100

    Create User.js

---
+++ backend/sec/models/User.js
@@ -0,0 +1,33 @@
+// src/models/User.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  reputation: {
+    type: Number,
+    default: 0
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
