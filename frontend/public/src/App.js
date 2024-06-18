c45fefa79e4ab58b0c46d0aaa65ca2702c941a10
Steve Aster AFOVO <72698973+SteveAsterAfovo@users.noreply.github.com>
2024-06-18 17:58:09 +0100

    Update App.js

--- frontend/public/src/App.js
+++ frontend/public/src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/marketplace" component={Marketplace} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <div className="app">
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/marketplace" component={Marketplace} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/about" component={About} />
+            <Route path="/privacy" component={Privacy} />
+            <Route path="/terms" component={Terms} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 };
