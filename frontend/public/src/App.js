f40e962e3ec7b48250c0d196dba80cfbc80325e0
Steve Aster AFOVO <72698973+SteveAsterAfovo@users.noreply.github.com>
2024-06-18 18:20:35 +0100

    Update App.js

--- frontend/public/src/App.js
+++ frontend/public/src/App.js
@@ -6,24 +6,18 @@ import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import Profile from './pages/Profile';
 import About from './pages/About';
-import Privacy from './pages/Privacy';
-import Terms from './pages/Terms';
 
 const App = () => {
   return (
     <Router>
       <div className="app">
         <Header />
-        <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/marketplace" component={Marketplace} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/about" component={About} />
-            <Route path="/privacy" component={Privacy} />
-            <Route path="/terms" component={Terms} />
-          </Switch>
-        </main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/marketplace" component={Marketplace} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/about" component={About} />
+        </Switch>
         <Footer />
       </div>
     </Router>
